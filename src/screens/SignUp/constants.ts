9dc5282be91a3e50b180e60e0ed786a587a28d4c
Elkin Bernal <elkin.bernal@accenture.com>
2021-11-18 17:37:18 -0500

    feat: create Sign Up Screen

---
+++ src/screens/SignUp/constants.ts
@@ -0,0 +1,7 @@
+export const FORM = [
+  { label: 'Name', key: 'first_name', type: 'text' },
+  { label: 'Last Name', key: 'last_name', type: 'text' },
+  { label: 'Email', key: 'email', type: 'text' },
+  { label: 'Password', key: 'password', type: 'password' },
+  { label: 'Password Confirmation', key: 'password_confirmation', type: 'password' }
+];
