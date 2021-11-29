41e6f3da887703804d8deca3c674222b83af0270
Elkin Bernal <elkin.bernal@accenture.com>
2021-11-29 13:00:10 -0500

    feat: sign in integration

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,4 +1,4 @@
-import { IUser, IUserResponse } from 'types/user.interface';
+import { IUser, IUserLogin, IUserLoginResponse, IUserResponse } from 'types/user.interface';
 
 import api from '../config/api';
 
@@ -7,4 +7,10 @@ const URL = '/api/v1/users';
 export const signUp = async (user: IUser | { locale: string }): Promise<IUserResponse> => {
   const { data } = await api.post<IUserResponse>(URL, user);
   return data as IUserResponse;
+};
+
+export const signIn = async (user: IUserLogin): Promise<IUserLoginResponse> => {
+  const { data, headers } = await api.post<IUserLoginResponse>(`${URL}/sign_in`, user);
+  const accessToken = headers ? headers['access-token'] : '';
+  return { ...data, accessToken } as IUserLoginResponse;
 };
