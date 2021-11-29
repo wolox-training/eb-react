c3735fd651e8c84c2b3358609f7484bcf5b7052f
Elkin Bernal <elkin.bernal@accenture.com>
2021-11-29 15:04:23 -0500

    feat: integrate Navbar

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-
 import 'scss/application.scss';
+import React from 'react';
+import { Route } from 'react-router-dom';
+
 import Home from 'screens/Home/index';
+import { Login } from 'screens/Login';
+import { SignUp } from 'screens/SignUp';
+
+import styles from './styles.module.scss';
 
 function App() {
-  return <Home />;
+  return (
+    <div className={`row middle center ${styles.app}`}>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route exact path="/home">
+        <Home />
+      </Route>
+      <Route exact path="/sign_up">
+        <SignUp />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
