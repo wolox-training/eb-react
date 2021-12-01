578bea84a95731e6b259b3738daf99b15b93d66a
Elkin Bernal <elkin.bernal@accenture.com>
2021-12-01 18:50:21 -0500

    feat: handle privacy of routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,25 +1,24 @@
-import 'scss/application.scss';
 import React from 'react';
-import { Route } from 'react-router-dom';
+import 'scss/application.scss';
+
+import { Switch } from 'react-router-dom';
 
 import Home from 'screens/Home/index';
 import { Login } from 'screens/Login';
 import { SignUp } from 'screens/SignUp';
+import { PrivateRoute } from 'components/PrivateRoute';
+import { PublicRoute } from 'components/PublicRoute';
 
 import styles from './styles.module.scss';
 
 function App() {
   return (
     <div className={`row middle center ${styles.app}`}>
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route exact path="/home">
-        <Home />
-      </Route>
-      <Route exact path="/sign_up">
-        <SignUp />
-      </Route>
+      <Switch>
+        <PublicRoute component={Login} exact path="/" />
+        <PublicRoute component={SignUp} exact path="/sign_up" />
+        <PrivateRoute component={Home} exact path="/home" />
+      </Switch>
     </div>
   );
 }
